ceee7856cdafa1c9c2c50a466e83ea36c0205269
al06208 <al06208@CEIT2552212X029.ad.georgiasouthern.edu>
2019-04-16 14:58:06 -0400

    added launcher

---
+++ OOD Battleship/src/battleship/Launcher.java
@@ -0,0 +1,30 @@
+package battleship;
+
+import java.util.Scanner;
+
+import javafx.application.Application;
+
+public class Launcher {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to battleship! What would you like to launch?\n1: Host\n2: Client\n3: Demo");
+		Scanner in = new Scanner(System.in);
+		int input = in.nextInt();
+		switch (input){
+			case 1:
+				System.out.println("Not yet implemented.");
+				break;
+			case 2:
+				System.out.println("Not yet implemented.");
+				break;
+			case 3: 
+				Application.launch(MainDemo.class);
+				break;
+			default:
+				System.out.println("what?");
+		}
+		in.close();
+	}
+	
+	
+}
