7a97822aedd93d644e814bf4ab6a26c3a73f3e16
griff <griff@Unique>
2019-05-01 16:58:41 -0400

    Submarine and upped to 5 ships

---
+++ OOD Battleship/src/battleship/Submarine.java
@@ -0,0 +1,34 @@
+package battleship;
+
+
+public class Submarine implements Ship{
+
+	private Space[] spaces;
+	public Submarine(Space[] spaces){
+		this.spaces = spaces;
+	}
+
+	@Override
+	public int shipSize() {
+		return 3;
+	}
+
+	@Override
+	public Space[] getSpaces() {
+		
+		return spaces;
+	}
+
+	
+
+	@Override
+	public boolean isDestroyed() {
+		for(Space space : spaces){
+			if(!space.isHit()){
+				return false;
+			}
+		}
+			return true;
+	}
+
+}
